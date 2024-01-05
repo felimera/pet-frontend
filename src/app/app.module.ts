2fa5cf2ccc072047a89b03a4392a4b13e34ee235
andres.mera <feli041089@gmail.com>
2024-01-05 13:59:39 -0500

    dev-sidenav : Creando y agregando todos los componentes necesarios para manejar el sidenav.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './modules/layout/body/body.component';
+import { SidenavComponent } from './modules/layout/sidenav/sidenav.component';
+import { DashboardComponent } from './modules/layout/dashboard/dashboard.component';
+import { ProductsComponent } from './modules/layout/products/products.component';
+import { StatisticsComponent } from './modules/layout/statistics/statistics.component';
+import { CoupensComponent } from './modules/layout/coupens/coupens.component';
+import { PagesComponent } from './modules/layout/pages/pages.component';
+import { MediaComponent } from './modules/layout/media/media.component';
+import { SettingsComponent } from './modules/layout/settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    SidenavComponent,
+    DashboardComponent,
+    ProductsComponent,
+    StatisticsComponent,
+    CoupensComponent,
+    PagesComponent,
+    MediaComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
