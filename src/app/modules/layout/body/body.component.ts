2148d53146671e320370a62cf3bd7d3e18389716
andres.mera <feli041089@gmail.com>
2024-01-08 09:59:23 -0500

    dev-sidenav : Desarrollo de sidenav responsive.

--- src/app/modules/layout/body/body.component.ts
+++ src/app/modules/layout/body/body.component.ts
@@ -14,11 +14,9 @@ export class BodyComponent {
     let styleClass = '';
     if (this.collapsed && this.screenWidth > 768) {
       styleClass = 'body-trimmed';
-    }
-    else if (this.collapsed && this.screenWidth <= 768 && this.screenWidth > 0) {
+    } else if (this.collapsed && this.screenWidth <= 768 && this.screenWidth > 0) {
       styleClass = 'body-md-screen';
     }
-    return '';
+    return styleClass;
   }
-
 }
