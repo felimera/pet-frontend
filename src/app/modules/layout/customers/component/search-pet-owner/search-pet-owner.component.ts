1bc46f6af7338623277a2f379e90a8237984c0a2
andres.mera <feli041089@gmail.com>
2024-01-16 10:54:32 -0500

    dev-customer : Implementando el servicio para obtener los customer del backend.

--- src/app/modules/layout/customers/component/search-pet-owner/search-pet-owner.component.ts
+++ src/app/modules/layout/customers/component/search-pet-owner/search-pet-owner.component.ts
@@ -3,6 +3,8 @@ import { FormControl } from '@angular/forms';
 import { Observable, map, startWith } from 'rxjs';
 import { _MatAutocompleteBase } from '@angular/material/autocomplete';
 import { CustomerDTO } from 'src/app/infrastructure/dto/customer.dto';
+import { CustomerService } from 'src/app/infrastructure/services/customer/customer.service';
+import { Customer } from 'src/app/core/models/customer.model';
 
 @Component({
   selector: 'app-search-pet-owner',
@@ -14,25 +16,38 @@ export class SearchPetOwnerComponent implements OnInit {
   valueRadio: string = 'G';
 
   myControl = new FormControl('');
-  // options: string[] = ['One', 'Two', 'Three'];
-  customerSearch: CustomerDTO[] = [
-    { id: 1, firstName: 'Andres Felipe', lastName: 'Mera', homeAddress: '', phone: '', email: '', isOlder: false, userId: 0 },
-    { id: 2, firstName: 'Manuela', lastName: 'Ramirez', homeAddress: '', phone: '', email: '', isOlder: false, userId: 0 }
-  ];
+  customerSearch: CustomerDTO[] = [];
   filteredOptions: Observable<string[]> | any;
   auto: _MatAutocompleteBase | any;
 
+  constructor(
+    private customerService: CustomerService
+  ) { }
+
   ngOnInit() {
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map(value => this._filter(value || '')),
-    );
+
+    this.customerService
+      .getAllCustomerByRole(Role.U)
+      .subscribe({
+        next: (customers: Customer[]) => {
+          customers.forEach(entity => {
+            this.customerSearch.push({ firstName: entity.firstName, lastName: entity.lastName, email: entity.email, homeAddress: entity.homeAddress, phone: entity.phone, isOlder: entity.isOlder, userId: entity.userId, id: entity.id });
+          });
+
+          this.filteredOptions = this.myControl.valueChanges.pipe(
+            startWith(''),
+            map(value => this._filter(value || '')),
+          );
+
+        },
+        error: (error: any) => console.log(error)
+      });
   }
 
   private _filter(value: string): CustomerDTO[] {
     const filterValue = value.toLowerCase();
 
-    return this.customerSearch.filter(option => this.combineFirstAndLastName(option).toLowerCase().includes(filterValue));
+    return this.customerSearch.filter((option: CustomerDTO) => this.combineFirstAndLastName(option).toLowerCase().includes(filterValue));
   }
 
   public combineFirstAndLastName(customerDTO: CustomerDTO): string {
