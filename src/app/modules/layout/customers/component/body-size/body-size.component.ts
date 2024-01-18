e4c40ca3be51bcc563394ca881f9fc8c791692b9
andres.mera <feli041089@gmail.com>
2024-01-18 10:24:26 -0500

    dev-customer : Construccion del componente body size.

--- src/app/modules/layout/customers/component/body-size/body-size.component.ts
+++ src/app/modules/layout/customers/component/body-size/body-size.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable, map, startWith } from 'rxjs';
+import { BodySize } from 'src/app/core/models/body-size.model';
+import { BodySizeDTO } from 'src/app/infrastructure/dto/body-size.dto';
+import { BodySizeService } from 'src/app/infrastructure/services/bodysize/body-size.service';
 
 @Component({
   selector: 'app-body-size',
@@ -7,9 +12,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BodySizeComponent implements OnInit {
 
-  constructor() { }
+  @Output() mgBodySizeEvent = new EventEmitter<number>();
+
+  myControl = new FormControl('');
+  bodySizeDTOs: BodySizeDTO[] = [];
+  filteredOptions!: Observable<BodySizeDTO[]>;
+
+  idBodySizeValue: number | undefined;
+
+  constructor(private bodySizeService: BodySizeService) { }
 
   ngOnInit() {
+    this.bodySizeService
+      .getAllBodySize()
+      .subscribe({
+        next: (bodySizes: BodySize[]) => {
+          bodySizes.forEach((data: BodySize) => {
+            this.bodySizeDTOs.push({ id: data.id, name: data.name, description: data.description, active: data.active });
+          })
+
+          this.filteredOptions = this.myControl.valueChanges.pipe(
+            startWith(''),
+            map(value => this._filter(value || '')),
+          );
+        },
+        error: (res: any) => console.log("error", res)
+      })
   }
 
+  private _filter(value: string): BodySizeDTO[] {
+    const filterValue = value.toLowerCase();
+
+    return this.bodySizeDTOs.filter((option: BodySizeDTO) => option.name.toLowerCase().includes(filterValue));
+  }
+
+  selectedBodySize(id: number): void {
+    this.idBodySizeValue = id;
+    this.mgBodySizeEvent.emit(this.idBodySizeValue);
+  }
 }
