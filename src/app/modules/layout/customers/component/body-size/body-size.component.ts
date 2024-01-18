1f337cb4d8a87616c68f35b23f218ac31c9940ae
andres.mera <feli041089@gmail.com>
2024-01-18 08:06:57 -0500

    dev-customer : Creando el component de Body-size.

---
+++ src/app/modules/layout/customers/component/body-size/body-size.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body-size',
+  templateUrl: './body-size.component.html',
+  styleUrls: ['./body-size.component.css']
+})
+export class BodySizeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
