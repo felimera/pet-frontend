4981d80e8e19f4fbf1e799b02d2dfc90e8a41c41
andres.mera <feli041089@gmail.com>
2024-01-18 07:48:25 -0500

    dev-customer : Modificacion casi finalizada de la plantilla y carga de los datos.

--- src/app/modules/layout/customers/component/color/color.component.ts
+++ src/app/modules/layout/customers/component/color/color.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable, map, startWith } from 'rxjs';
+import { Color } from 'src/app/core/models/color.model';
+import { ColorDTO } from 'src/app/infrastructure/dto/color.dto';
+import { ColorService } from 'src/app/infrastructure/services/color/color.service';
 
 @Component({
   selector: 'app-color',
@@ -9,19 +12,34 @@ import { Observable, map, startWith } from 'rxjs';
 })
 export class ColorComponent implements OnInit {
   myControl = new FormControl('');
-  colors: string[] = ['One', 'Two', 'Three'];
-  filteredOptions!: Observable<string[]>;
+  colorDTOs: ColorDTO[] = [];
+  filteredOptions!: Observable<ColorDTO[]>;
+
+  constructor(private colorService: ColorService) { }
 
   ngOnInit() {
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map(value => this._filter(value || '')),
-    );
+
+    this.colorService
+      .getColorAll()
+      .subscribe({
+        next: (colors: Color[]) => {
+          colors.forEach((data: Color) => {
+            this.colorDTOs.push({ id: data.id, name: data.name, code: data.code, active: data.active });
+          })
+
+          this.filteredOptions = this.myControl.valueChanges.pipe(
+            startWith(''),
+            map(value => this._filter(value || '')),
+          );
+        },
+        error: (res: any) => console.log("error", res)
+      })
+
   }
 
-  private _filter(value: string): string[] {
+  private _filter(value: string): ColorDTO[] {
     const filterValue = value.toLowerCase();
 
-    return this.colors.filter(option => option.toLowerCase().includes(filterValue));
+    return this.colorDTOs.filter((option: ColorDTO) => option.name.toLowerCase().includes(filterValue));
   }
 }
