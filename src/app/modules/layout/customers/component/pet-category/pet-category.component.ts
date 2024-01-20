bfaac351c4192e5788962c5e2aa3725bd21db293
andres.mera <feli041089@gmail.com>
2024-01-20 09:34:15 -0500

    dev-customer : Creacion del componente de petCategory.

---
+++ src/app/modules/layout/customers/component/pet-category/pet-category.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-category',
+  templateUrl: './pet-category.component.html',
+  styleUrls: ['./pet-category.component.css']
+})
+export class PetCategoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
