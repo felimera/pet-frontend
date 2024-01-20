6774b4dc63b8c3915b801f3f70c800dc658614f2
andres.mera <feli041089@gmail.com>
2024-01-20 09:43:36 -0500

    dev-customer : Desarrollado todo el componete de Pet Category.

--- src/app/modules/layout/customers/component/pet-category/pet-category.component.ts
+++ src/app/modules/layout/customers/component/pet-category/pet-category.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable, map, startWith } from 'rxjs';
+import { PetCategory } from 'src/app/core/models/pet-category.model';
+import { PetCategoryDTO } from 'src/app/infrastructure/dto/pet-category.dto';
+import { PetCategoryService } from 'src/app/infrastructure/services/petcategory/pet-category.service';
 
 @Component({
   selector: 'app-pet-category',
@@ -7,9 +12,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PetCategoryComponent implements OnInit {
 
-  constructor() { }
+  @Output() mgPetCategoryEvent = new EventEmitter<number>();
+
+  myControl = new FormControl('');
+  petCategoryDTOs: PetCategoryDTO[] = [];
+  filteredOptions!: Observable<PetCategoryDTO[]>;
+
+  idPetCategoryValue: number | undefined;
+
+  constructor(private petCategoryService: PetCategoryService) { }
 
   ngOnInit() {
+    this.petCategoryService
+      .getAllPetCategory()
+      .subscribe({
+        next: (petCategories: PetCategory[]) => {
+          petCategories.forEach((data: PetCategory) => {
+            this.petCategoryDTOs.push({ id: data.id, name: data.name, description: data.description, active: data.active });
+          })
+
+          this.filteredOptions = this.myControl.valueChanges.pipe(
+            startWith(''),
+            map(value => this._filter(value || '')),
+          );
+        },
+        error: (res: any) => console.log("error", res)
+      })
   }
 
+  private _filter(value: string): PetCategoryDTO[] {
+    const filterValue = value.toLowerCase();
+
+    return this.petCategoryDTOs.filter((option: PetCategoryDTO) => option.name.toLowerCase().includes(filterValue));
+  }
+
+  selectedPetCategory(id: number): void {
+    this.idPetCategoryValue = id;
+    this.mgPetCategoryEvent.emit(this.idPetCategoryValue);
+  }
 }
