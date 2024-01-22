5f0dde00042ebf0b407d7595e9e3eca8ec53374a
andres.mera <feli041089@gmail.com>
2024-01-22 10:13:54 -0500

    dev-media : Creacion del componente de media.

---
+++ src/app/modules/layout/customers/component/register-pet-media/register-pet-media.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-pet-media',
+  templateUrl: './register-pet-media.component.html',
+  styleUrls: ['./register-pet-media.component.css']
+})
+export class RegisterPetMediaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
