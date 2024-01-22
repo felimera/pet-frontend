b128064d92922815392d0973e1faa3f56e32bb3e
andres.mera <feli041089@gmail.com>
2024-01-22 10:35:57 -0500

    dev-media : Modificacion de service media.

--- src/app/infrastructure/services/media/media.service.ts
+++ src/app/infrastructure/services/media/media.service.ts
@@ -1,10 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MediaService {
 
-constructor() { }
+  private apiUrl = 'http://localhost:83/api/media';
+
+  constructor(private http: HttpClient) { }
+
+  uploadFile(formData: FormData): Observable<any> {
+    return this.http.post<any>(this.apiUrl, formData);
+  }
 
 }
