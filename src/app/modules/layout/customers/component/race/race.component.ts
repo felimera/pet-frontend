1ffa7975b165286696d48bad9ceef362b793da09
andres.mera <feli041089@gmail.com>
2024-01-26 07:38:10 -0500

    dev-media : Desarrollo del componete race.

--- src/app/modules/layout/customers/component/race/race.component.ts
+++ src/app/modules/layout/customers/component/race/race.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable, map, startWith } from 'rxjs';
+import { Race } from 'src/app/core/models/race.model';
+import { RaceDTO } from 'src/app/infrastructure/dto/race.dto';
+import { RaceService } from 'src/app/infrastructure/services/race/race.service';
 
 @Component({
   selector: 'app-race',
@@ -7,9 +12,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RaceComponent implements OnInit {
 
-  constructor() { }
+  @Output() mgRaceEvent = new EventEmitter<number>();
+
+  myControl = new FormControl('');
+  raceDTOs: RaceDTO[] = [];
+  filteredOptions!: Observable<RaceDTO[]>;
+
+  idRaceValue: number | undefined;
+
+  constructor(private raceService: RaceService) { }
 
   ngOnInit() {
+    this.raceService
+      .getAllRace()
+      .subscribe({
+        next: (races: Race[]) => {
+          races.forEach((data: Race) => {
+            this.raceDTOs.push({ id: data.id, name: data.name, description: data.description, active: data.active });
+          })
+
+          this.filteredOptions = this.myControl.valueChanges.pipe(
+            startWith(''),
+            map(value => this._filter(value || '')),
+          );
+        },
+        error: (res: any) => console.log("error", res)
+      })
+  }
+
+  private _filter(value: string): RaceDTO[] {
+    const filterValue = value.toLowerCase();
+
+    return this.raceDTOs.filter((option: RaceDTO) => option.name.toLowerCase().includes(filterValue));
+  }
+
+  selectedRaceValue(id: number): void {
+    this.idRaceValue = id;
+    this.mgRaceEvent.emit(this.idRaceValue);
   }
 
 }
